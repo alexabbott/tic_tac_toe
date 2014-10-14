f3088569fc5e6a07ba795853732c20741c440fcf
Alexander Abbott <Alex@Alexanders-MacBook-Pro.local>
2014-10-14 15:28:31 -0700

    Update variable names for best practices

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var app = angular.module('toeApp', []);
+var app = angular.module('ToeApp', []);
 
   app.controller('GameController', function($scope) {
 
@@ -7,11 +7,11 @@ var app = angular.module('toeApp', []);
     $scope.playerO = [] ;
 
     // create 'movecounter' variable to keep track of how many moves have been made
-    $scope.movecounter = 0 ;
+    $scope.moveCounter = 0 ;
 
     // create variables to keep track of how many wins each player has accumulated
-    $scope.xwincounter = 0 ;
-    $scope.owincounter = 0 ;
+    $scope.xWinCounter = 0 ;
+    $scope.oWinCounter = 0 ;
 
     // create array of all possible winning combo arrays
     $scope.winCombos = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
@@ -30,7 +30,7 @@ var app = angular.module('toeApp', []);
       ]  ;
 
     // used for the reset button
-    $scope.gamecounter = 0;
+    $scope.gameCounter = 0;
 
     // create 'winner' variable to display winner on winner declaration
     $scope.winner = 0;
@@ -42,11 +42,11 @@ var app = angular.module('toeApp', []);
           if ( $scope.playerX.indexOf($scope.winCombos[i][1]) >= 0 ) {
             if ( $scope.playerX.indexOf($scope.winCombos[i][2]) >= 0 ) {
               // Increase x's win counter by 1
-              $scope.xwincounter = $scope.xwincounter + 1;
+              $scope.xWinCounter = $scope.xWinCounter + 1;
               // Define 'X' as the winner to be displayed in winner declaration
               $scope.winner = "X";
               // Increase gamecounter by one to show hidden winner declaration div
-              $scope.gamecounter = $scope.gamecounter + 1;
+              $scope.gameCounter = $scope.gameCounter + 1;
               // alert("X wins! X has " + $scope.xwincounter + " wins and " + "O has " + $scope.owincounter + " wins");
               return true;
             }
@@ -62,11 +62,11 @@ var app = angular.module('toeApp', []);
           if ( $scope.playerO.indexOf($scope.winCombos[i][1]) >= 0 ) {
             if ( $scope.playerO.indexOf($scope.winCombos[i][2]) >= 0 ) {
               // Increase o's win counter by 1
-              $scope.owincounter = $scope.owincounter + 1;
+              $scope.oWinCounter = $scope.oWinCounter + 1;
               // Define 'O' as the winner to be displayed in winner declaration
               $scope.winner = "O";
               // Increase gamecounter by one to show hidden winner declaration div
-              $scope.gamecounter = $scope.gamecounter + 1;
+              $scope.gameCounter = $scope.gameCounter + 1;
               // alert("O wins! O has " + $scope.owincounter + " wins and " + "X has " + $scope.xwincounter + " wins");
               return true;
             }
@@ -77,17 +77,17 @@ var app = angular.module('toeApp', []);
 
     // on each click, increase movecounter by one, change cell status based on movecounter, and run the checkWin function
     $scope.playerPicks = function(thisCell) {
-      $scope.movecounter = $scope.movecounter + 1 ;
-      if (($scope.movecounter % 2) == 1) {
+      $scope.moveCounter = $scope.moveCounter + 1 ;
+      if (($scope.moveCounter % 2) == 1) {
         $scope.playerX.push(thisCell.status);
         $scope.playerX.sort();
         // set the new status for the cell, which will trigger a change to the xbox class
         thisCell.status = "X" ;
         $scope.checkWinX();
         // show the 'draw' status if neither player has a winning combo after the 9 possible clicks
-        if (($scope.movecounter == 9) && ($scope.winner != "X")) {
+        if (($scope.moveCounter == 9) && ($scope.winner != "X")) {
           // Increase gamecounter by one to show hidden 'draw' declaration div
-          $scope.gamecounter = $scope.gamecounter + 1;
+          $scope.gameCounter = $scope.gameCounter + 1;
           $scope.winner = "NO ONE";
           // alert("It's a draw");
         }  
@@ -106,8 +106,8 @@ var app = angular.module('toeApp', []);
     $scope.reset = function() {
       $scope.playerX = [];
       $scope.playerO = [];
-      $scope.movecounter = 0;
-      $scope.gamecounter = 0;
+      $scope.moveCounter = 0;
+      $scope.gameCounter = 0;
       $scope.winner = 0;
       $scope.cellList = [
       {status: 1}, 
